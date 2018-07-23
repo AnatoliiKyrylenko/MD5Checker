43265e5821c6ece887317b222a542c949fb47f56
AnatoliiKyrylenko <KyrylenkoAnatolii@gmail.com>
2018-07-23 22:52:51 +0300

    a little patch:)

--- md5checker.sh
+++ md5checker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # EN version
 
-echo Enter md5sum
+echo "Enter md5sum"
 read a
-echo Enter md5sum for cheking
+echo "Enter md5sum for cheking"
 read b
-if [ $a == $b ]; then echo $a and $b -\> Ok \(:; fi
-if [ $a != $b ]; then echo $a and $b -\> No, something is wrong! \):; fi
+if [ $a == $b ]; then echo $a and $b -\> "Ok, It's all right! (:"; fi
+if [ $a != $b ]; then echo $a and $b -\> "No, something is wrong! ):"; fi
