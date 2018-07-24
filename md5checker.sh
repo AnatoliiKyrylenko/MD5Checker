9f3ff5a00d41b013b3f53ebcb3c3b14487a90e28
AnatoliiKyrylenko <KyrylenkoAnatolii@gmail.com>
2018-07-24 17:52:35 +0300

    Comment mod

--- md5checker.sh
+++ md5checker.sh
@@ -5,5 +5,7 @@ echo "Enter md5sum"
 read a
 echo "Enter md5sum for cheking"
 read b
+# If equal
 if [ $a == $b ]; then echo $a and $b -\> "Ok, It's all right! (:"; fi
+# If don't equal
 if [ $a != $b ]; then echo $a and $b -\> "No, something is wrong! ):"; fi
